38908febeee0326df517a709d5dcea9b95a06887
BouranjaOthmane <bouranjaothmane@gmail.com>
2026-02-04 12:54:48 +0100

    Push

---
+++ resources/views/admin/tabs/salaries.blade.php
@@ -0,0 +1,19 @@
+{{-- resources/views/admin/employees/index.blade.php --}}
+@extends('adminlte::page')
+
+@section('title', 'Documents')
+
+@section('content_header')
+    <div class="d-flex justify-content-between align-items-center">
+        <h1 class="mb-0">Documents</h1>
+
+        <a href="{{ route('admin.employees.create') }}" class="btn btn-primary">
+            <i class="fas fa-user-plus mr-1"></i> Add Documents
+        </a>
+    </div>
+@endsection
+
+@section('content')
+
+  
+@endsection
