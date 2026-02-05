200afda36f65aed8535603d322a5749450f18aa8
BouranjaOthmane <bouranjaothmane@gmail.com>
2026-02-05 13:26:46 +0100

    Push

--- app/Http/Controllers/Admin/HolidayController.php
+++ app/Http/Controllers/Admin/HolidayController.php
@@ -12,7 +12,7 @@ class HolidayController extends Controller
      */
     public function index()
     {
-        //
+        
     }
 
     /**
@@ -20,7 +20,7 @@ class HolidayController extends Controller
      */
     public function create()
     {
-        //
+        
     }
 
     /**
@@ -28,7 +28,7 @@ class HolidayController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        
     }
 
     /**
@@ -36,7 +36,7 @@ class HolidayController extends Controller
      */
     public function show(string $id)
     {
-        //
+        
     }
 
     /**
@@ -44,7 +44,7 @@ class HolidayController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        
     }
 
     /**
@@ -52,7 +52,7 @@ class HolidayController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        
     }
 
     /**
@@ -60,6 +60,6 @@ class HolidayController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        
     }
 }
